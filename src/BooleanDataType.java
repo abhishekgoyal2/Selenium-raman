2e67ebb96e221588291efaab17f971df35c8529a
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-18 18:18:41 +0530

    boolean in progress

---
+++ src/BooleanDataType.java
@@ -0,0 +1,36 @@
+
+public class BooleanDataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean bol =true;
+		System.out.println(10<20);
+		
+		int first_no=30;
+		int second_no=20;
+		
+		boolean result =first_no>second_no;
+		System.out.println(result);
+		
+		if (first_no>second_no)
+		{
+			System.out.println("first number is greater");
+
+		}
+		
+		else if(second_no>first_no)
+		{
+			System.out.println("second number is greater");
+
+		}
+		
+		else
+		{
+			System.out.println("both are equal");
+
+		}
+		
+	}
+
+}
