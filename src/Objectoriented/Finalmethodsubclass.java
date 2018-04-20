8a047ff5f5f965c48562e6dce12443596c30c7bc
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-20 12:40:31 +0530

    Final method cannot be overiden or reinstalised variable

---
+++ src/Objectoriented/Finalmethodsubclass.java
@@ -0,0 +1,16 @@
+package Objectoriented;
+
+public class Finalmethodsubclass extends Finalmethod{
+
+//	public  final int display(int b)
+//	{
+//		System.out.println("final method 1 with no parameter ");
+//		
+//	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
