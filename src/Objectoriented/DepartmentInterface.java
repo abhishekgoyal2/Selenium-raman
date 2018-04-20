213505ef23f0391947218e862a8367fb2b574309
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-20 11:30:59 +0530

    Team commit Interface

---
+++ src/Objectoriented/DepartmentInterface.java
@@ -0,0 +1,39 @@
+package Objectoriented;
+
+public class DepartmentInterface implements Employee{
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e =new DepartmentInterface();
+		e.department();
+		e.salary();
+		e.hrpolicy();
+	}
+
+	@Override
+	public void salary() {
+		// TODO Auto-generated method stub
+		System.out.print("prints employee salary");
+	}
+
+	@Override
+	public void department() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void emplname() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hrpolicy() {
+		// TODO Auto-generated method stub
+		System.out.print("Hr policy applies");
+	}
+
+}
---
+++ src/Objectoriented/Employee.java
@@ -0,0 +1,15 @@
+package Objectoriented;
+
+
+
+public interface Employee {
+
+	int salary =0;
+	String department ="null";
+	
+	public  void salary() ;
+	public void department();
+	public void emplname();
+	
+	public void hrpolicy();
+}
