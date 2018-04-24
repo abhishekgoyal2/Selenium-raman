a00306fcd5647cadcf294e8c893a77d6216b8e86
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-24 15:36:10 +0530

    multiple constructor

---
+++ src/Objectoriented/BoxConstructor.java
@@ -0,0 +1,47 @@
+package Objectoriented;
+
+public class BoxConstructor {
+	
+	 double width, height, depth;
+	 
+	    // constructor used when all dimensions
+	    // specified
+	    BoxConstructor(double w, double h, double d)
+	    {
+	        width = w;
+	        height = h;
+	        depth = d;
+	    }
+	 
+	    // constructor used when no dimensions
+	    // specified
+	    BoxConstructor()
+	    {
+	        width = height = depth = 0;
+	    }
+	 
+	    // constructor used when cube is created
+	    BoxConstructor(double len)
+	    {
+	        width = height = depth = len;
+	    }
+	 
+	    // compute and return volume
+	    double volume()
+	    {
+	        return width * height * depth;
+	    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+BoxConstructor box1=new BoxConstructor();
+BoxConstructor box2=new BoxConstructor(2.4);
+BoxConstructor box3=new BoxConstructor(2,4,5);
+
+double vol =box3.volume();
+
+System.out.println(vol);
+//	
+	}
+
+}
