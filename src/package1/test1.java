5f76d09744fda920b1adefe46200fb22a12be7a1
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-26 18:08:19 +0530

    Protected private

---
+++ src/package1/test1.java
@@ -0,0 +1,15 @@
+package package1;
+
+ public class test1 {
+ public int sum,a,b;
+	 protected int add(int a,int b)
+	 {
+		sum=a+b;
+		return sum;
+	 }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/package2/test2.java
@@ -0,0 +1,13 @@
+package package2;
+
+import package1.test1;
+
+public class test2 extends test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+test1 default1=new test1();
+
+	}
+
+}
