fb03c47884e29ae49d3c552ee03f3743cd67ed7e
abhishek.goyal2 <abhi25goel@gmail.com>
2018-04-27 15:24:49 +0530

    privcheck

---
+++ src/package1/PrivcheckA.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class PrivcheckA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PrivcheckA pa= new PrivcheckA();
+		privCheck pc =new privCheck();
+		pc.add();
+		
+	
+	}
+
+}
---
+++ src/package1/privCheck.java
@@ -0,0 +1,19 @@
+package package1;
+
+   class privCheck {
+
+	   private static  int a;
+	   private static  int b;
+	 
+	        void add()
+	   {
+		System.out.println("this is add function");
+	   }
+	   
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	}
+
+}
